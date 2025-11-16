d23233745e225f7547b54d87ffbe4936af6ae858
Mark Taylor <mark@kaizen.edu>
2025-11-16 21:29:43 +0100

    Add CSP headers to allow GHL calendars and voice AI widget

--- next.config.js
+++ next.config.js
@@ -1,6 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://link.morpheus8bedford.co.uk https://voice.morpheus8bedford.co.uk https://connect.facebook.net",
+              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+              "font-src 'self' https://fonts.gstatic.com",
+              "img-src 'self' data: https: blob:",
+              "frame-src 'self' https://link.morpheus8bedford.co.uk https://voice.morpheus8bedford.co.uk https://www.facebook.com",
+              "connect-src 'self' https://link.morpheus8bedford.co.uk https://voice.morpheus8bedford.co.uk https://www.facebook.com",
+              "media-src 'self' blob:",
+            ].join('; '),
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
